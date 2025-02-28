31a1c82239cf24a0fefab09eeafde6799a576a2b
Ben-1819 <ben.brown20332@gmail.com>
2025-02-28 08:39:40 +0000

    Created BookingController and added logic for bookings Added in booking routes

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use App\Models\User;
+use App\Models\Property;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        log::info("Get all bookings");
+        $all_bookings = Booking::all();
+        log::info("Pass all bookings into bookings.index");
+        return view("booking.index", compact("all_bookings"));
+    }
+
+    public function usersBookings(){
+        log::info("Get all bookings where the customer_id matches the users id");
+        $users_bookings = Booking::where("customer_id", request()->user()->id);
+        log::info("Pass users bookings into bookings.users");
+        return view("booking.users", compact("users_bookings"));
+    }
+
+    public function hostsBookings(){
+        log::info("Get all bookings where the host_id matches the users id");
+        $users_bookings = Booking::where("host_id", request()->user()->id);
+        log::info("Pass users bookings into bookings.hosts");
+        return view("bookings.hosts", compact("users_bookings"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create($id)
+    {
+        $property = Property::find($id);
+        log::info("Returning booking.create view");
+        return view("booking.create", compact ("property"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        log::info("Validate the input");
+        $request->validate([
+            "host_id" => ["required", "integer"],
+            "property_id" => ["required", "integer"],
+            "amount_of_guests" => ["required", "integer"],
+            "amount_of_pets" => ["required", "integer"],
+            "booking_start" => ["required", "date"],
+            "booking_end" => ["required", "date"],
+        ]);
+        $property = Property::find($request->property_id);
+
+        $amount_of_nights = (Carbon::parse($request->booking_start)->diffInDays(Carbon::parse($request->booking_end)));
+        //(bcmul($property->price_per_pet, strval($request->amount_of_pets), 2)),
+        //(bcmul(strval($amount_of_nights), $property->price_per_night, 2)) + (bcmul($property->price_per_pet, strval($request->amount_of_pets), 2)),
+        log::info("Create new booking");
+        $booking = new Booking([
+            "host_id" => $request->host_id,
+            "customer_id" => request()->user()->id,
+            "property_id" => $property->id,
+            "amount_of_guests" => $request->amount_of_guests,
+            "amount_of_pets" => $request->amount_of_pets,
+            "extra_charges" => strval((doubleval($property->price_per_pet) * doubleval($request->amount_of_pets))),
+            "booking_cost" => strval((doubleval($amount_of_nights) * doubleval($property->price_per_night)) + (doubleval($property->price_per_pet) * doubleval($request->amount_of_pets))),
+            "booking_start" => $request->booking_start,
+            "booking_end" => $request->booking_end,
+        ]);
+        $booking->save();
+        log::info("Booking Saved!");
+        log::info("Host on booking: {$booking->host_id}");
+        log::info("Customer on booking: {$booking->customer_id}");
+        log::info("Property on booking: {$booking->property_id}");
+        log::info("Amount of guests on booking: {$booking->amount_of_guests}");
+        log::info("Amount of pets on booking: {$booking->amount_of_pets}");
+        log::info("Extra charges on booking: {$booking->extra_charges}");
+        log::info("Total Cost of Booking: {$booking->booking_cost}");
+        log::info("Booking start date: {$booking->booking_start}");
+        log::info("Booking end date: {$booking->booking_end}");
+
+        log::info("Returning to booking.show view");
+        $id = $booking->id;
+        return redirect()->route("booking.show", compact("id"));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Booking $booking, $id)
+    {
+        //Stop users who did not create the booking or own the property from viewing the booking
+        log::info("Retrieve the record from the booking table that has the same ID as the one passed");
+        $booking = Booking::find($id);
+        if($booking->customer_id == request()->user()->id){
+            log::info("Return booking.show view");
+            return view("booking.show", compact("booking"));
+        }
+        elseif($booking->host_id == request()->user()->id){
+            log::info("Return booking.show view");
+            return view("booking.show", compact("booking"));
+        }
+        else{
+            log::info("User tying to view the booking was not the creator of the booking or the owner of the property");
+            abort(403);
+        }
+
+        //$booking = Booking::find($id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Booking $booking, $id)
+    {
+        //Stop users who did not create the booking or own the property from editing the booking
+        log::info("Retrieve the record from the booking table that has the same ID as the one passed");
+        $booking = Booking::find($id);
+        if($booking->customer_id == request()->user()->id){
+            log::info("Return booking.edit view");
+            return view("booking.edit", compact("booking"));
+        }
+        elseif($booking->host_id == request()->user()->id){
+            log::info("Return booking.edit view");
+            return view("booking.edit", compact("booking"));
+        }
+        else{
+            log::info("User tying to edit the booking was not the creator of the booking or the owner of the property");
+            abort(403);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Booking $booking)
+    {
+        //Stop users who did not create the booking or own the property from updating the booking
+        if($booking->customer_id == request()->user()->id){
+
+        }
+        elseif($booking->host_id == request()->user()->id){
+
+        }
+        else{
+            log::info("User tying to update the booking was not the creator of the booking or the owner of the property");
+            abort(403);
+        }
+
+        log::info("Validate the input");
+        $request->validate([
+            "host_id" => ["required", "integer"],
+            "property_id" => ["required", "integer"],
+            "amount_of_guests" => ["required", "integer"],
+            "amount_of_pets" => ["required", "integer"],
+            "booking_start" => ["required", "date"],
+            "booking_end" => ["required", "date"],
+        ]);
+        log::info("Get the record belonging to the property on the booking from the properties table");
+        //$property = Property::find($request->property_id);
+
+        $amount_of_nights = (Carbon::parse($request->booking_start)->diffInDays(Carbon::parse($request->booking_end)) - 1);
+
+        $property = Property::find($booking->property_id);
+        log::info("Update the booking");
+        $update_booking = Booking::find($booking->id)->update(attributes: [
+            "host_id" => $request->host_id,
+            "customer_id" => request()->user()->id,
+            "property_id" => $property->id,
+            "amount_of_guests" => $request->amount_of_guests,
+            "amount_of_pets" => $request->amount_of_pets,
+            "extra_charges" => strval((doubleval($property->price_per_pet) * doubleval($request->amount_of_pets))),
+            "booking_cost" => strval((doubleval($amount_of_nights) * doubleval($property->price_per_night)) + (doubleval($property->price_per_pet) * doubleval($request->amount_of_pets))),
+            "booking_start" => $request->booking_start,
+            "booking_end" => $request->booking_end,
+        ]);
+        log::info("Booking updated!");
+        log::info("Host on booking: {$update_booking->host_id}");
+        log::info("Customer on booking: {$booking->customer_id}");
+        log::info("Property on booking: {$booking->property_id}");
+        log::info("Amount of guests on booking: {$booking->amount_of_guests}");
+        log::info("Amount of pets on booking: {$booking->amount_of_pets}");
+        log::info("Extra charges on booking: {$booking->extra_charges}");
+        log::info("Total Cost of Booking: {$booking->booking_cost}");
+        log::info("Booking start date: {$booking->booking_start}");
+        log::info("Booking end date: {$update_booking->booking_end}");
+
+        log::info("Returning to booking.show view");
+        $id = $update_booking->id;
+        return redirect()->route("booking.show", compact("id"));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Booking $booking)
+    {
+        //Stop users who did not create the booking or own the property from deleting the booking
+        if($booking->customer_id == request()->user()->id){
+
+        }
+        elseif($booking->host_id == request()->user()->id){
+
+        }
+        else{
+            log::info("User tying to delete the booking was not the creator of the booking or the owner of the property");
+            abort(403);
+        }
+        log::info("Delete the record from the booking table that has the same id as the one passed");
+        Booking::where("id", $booking->id)->delete();
+        log::info("Booking Deleted, returning to dashboard");
+        return redirect()->route("dashboard");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\AmenitiesController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,17 +20,19 @@ Route::middleware('auth')->group(function () {
     Route::get("/profile/show", [ProfileController::class, "show"])->name("profile.show");
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth', 'verified']);
 
 Route::name("property.")->prefix("/property")->group(function(){
     Route::get("", [PropertyController::class, "index"])->name("index");
-    Route::get("/create", [PropertyController::class, "create"])->name("create");
-    Route::post("", [PropertyController::class, "store"])->name("store");
-    Route::get("/owned", [PropertyController::class, "usersProperties"])->name("owned");
+    Route::get("/create", [PropertyController::class, "create"])->name("create")->middleware(['auth', 'verified']);
+    Route::post("", [PropertyController::class, "store"])->name("store")->middleware(['auth', 'verified']);
+    Route::get("/owned", [PropertyController::class, "usersProperties"])->name("owned")->middleware(['auth', 'verified']);
     Route::get("/{id}", [PropertyController::class, "show"])->name("show");
-    Route::get("/{id}/edit", [PropertyController::class, "edit"])->name("edit");
-    Route::put("/{id}", [PropertyController::class, "update"])->name("update");
-    Route::delete("/{id}", [PropertyController::class, "destroy"])->name("destroy");
+    Route::get("/{id}/edit", [PropertyController::class, "edit"])->name("edit")->middleware(['auth', 'verified']);
+    Route::put("/{id}", [PropertyController::class, "update"])->name("update")->middleware(['auth', 'verified']);
+    Route::delete("/{id}", [PropertyController::class, "destroy"])->name("destroy")->middleware(['auth', 'verified']);
+    Route::patch("{/{id}/addReview", [PropertyController::class, "addReview"])->name("addReview");
+    Route::patch("/{id}/getAverage", [PropertyController::class, "averageRating"])->name("averageRatin");
 });
 
 Route::name("amenity.")->prefix("/amenity")->group(function(){
@@ -39,6 +43,27 @@ Route::name("amenity.")->prefix("/amenity")->group(function(){
     Route::delete("/{id}", [AmenitiesController::class, "destroyAll"])->name("destroyAll");
     Route::get("/{id}/list", [AmenitiesController::class, "list"])->name("list");
     Route::delete("/{id}/delete", [AmenitiesController::class, "delete"])->name("destroy");
+})->middleware(['auth', 'verified']);
+
+Route::name("booking.")->prefix("/booking")->group(function(){
+    Route::get("", [BookingController::class, "index"])->name("index");
+    Route::get("/{id}/create", [BookingController::class, "create"])->name("create");
+    Route::post("", [BookingController::class, "store"])->name("store");
+    Route::get("/{id}", [BookingController::class, "show"])->name("show");
+    Route::get("/{id}/edit", [BookingController::class, "edit"])->name("edit");
+    Route::put("/{id}", [BookingController::class, "update"])->name("update");
+    Route::delete("/{id}", [BookingController::class, "destroy"])->name("destroy");
+})->middleware(['auth', 'verified']);
+
+Route::name("review.")->prefix("/review")->group(function(){
+    Route::get("", [ReviewController::class, "index"])->name("index");
+    Route::get("/create", [ReviewController::class, "create"])->name("create");
+    Route::post("", [ReviewController::class, "store"])->name("store");
+    Route::get("/{id}", [ReviewController::class, "show"])->name("show");
+    Route::get("/{id}/edit", [ReviewController::class, "edit"])->name("edit");
+    Route::put("/{id}", [ReviewController::class, "update"])->name("update");
+    Route::delete("/{id}", [ReviewController::class, "destroy"])->name("destroy");
 });
+
 
 require __DIR__.'/auth.php';
