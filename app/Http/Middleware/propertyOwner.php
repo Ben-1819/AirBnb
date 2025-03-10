bceb281808bcc67f1e077f9d6379491d41d25380
Ben-1819 <ben.brown20332@gmail.com>
2025-03-10 14:19:50 +0000

    feat: Created middleware that only allows the owner of a property or a superadmin to edit it

---
+++ app/Http/Middleware/propertyOwner.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\Property;
+
+class propertyOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $propertyId = $request->route("id");
+        log::info("Property id: {id}", ["id" => $propertyId]);
+        $property = Property::find($propertyId);
+
+        if($property->owner_id == request()->user()->id){
+            return $next($request);
+        }
+        elseif(!$property || request()->user()->withoutRole("superadmin")){
+            return $next($request);
+        }
+        else{
+            abort(403, "Not authorised to access this page");
+        }
+    }
+}
