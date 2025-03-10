5f9b076ea01ecc931684d5d7d03453211ebdb26b
Ben-1819 <ben.brown20332@gmail.com>
2025-03-10 12:19:58 +0000

    fix: Removed broken JavaScript

--- resources/views/location/create.blade.php
+++ resources/views/location/create.blade.php
@@ -3,23 +3,3 @@
         <store-location/>
     </div>
 </x-app-layout>
-
-
-    <!--/*window.addEventListener('load', () => {
-        mapboxsearch.config.accessToken = ACCESS_TOKEN;
-
-        // Add confirmation prompt to shipping address
-        const form = document.querySelector("form");
-        form.addEventListener("submit", async (e) => {
-            e.preventDefault();
-            const result = await mapboxsearch.confirmAddress(form, {
-                theme: { variables: { border: '3px solid rgba(0,0,0,0.35)', borderRadius: '18px' } },
-                minimap: {
-                    defaultMapStyle: ['mapbox', 'outdoors-v11'],
-                    satelliteToggle: true
-                },
-                skipConfirmModal: (feature) => false // overrides default behavior, show dialog every time
-            });
-            console.log(result);
-        });
-    });-->
